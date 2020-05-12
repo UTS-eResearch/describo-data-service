5acfd95112a9ef6d4bd9a753816da8151689af55
Marco La Rosa <m@lr.id.au>
2020-05-12 11:28:46 +1000

    implement method to verify load data and test

--- src/database.js
+++ src/database.js
@@ -1,6 +1,6 @@
 const { Sequelize, Model, DataTypes } = require("sequelize");
-let modules = [require("./data.model")];
-const models = [];
+const { has, isArray, isPlainObject } = require("lodash");
+let models = [require("./data.model")];
 
 class Database {
     constructor({ databaseFile }) {
@@ -11,8 +11,8 @@ class Database {
         });
 
         // Initialize models
-        modules.forEach((module) => {
-            module(sequelize);
+        models.forEach((model) => {
+            model(sequelize);
         });
         this.sequelize = sequelize;
     }
@@ -20,6 +20,45 @@ class Database {
     async connect() {
         await this.sequelize.sync();
         return this.sequelize.models;
+    }
+
+    async load({ data }) {
+        return this.verifyInputData({ data });
+    }
+
+    verifyInputData({ data }) {
+        if (!isArray(data)) {
+            throw new Error(`Data must be an array`);
+        }
+        for (let entry of data) {
+            if (!isPlainObject(entry)) {
+                throw new Error(`Each entry in the data must be an object`);
+            }
+            if (!has(entry, "@id"))
+                throw new Error(
+                    `Each entry in the data must have an @id property`
+                );
+            if (!has(entry, "@type"))
+                throw new Error(
+                    `Each entry in the data must have an @type property`
+                );
+            if (!has(entry, "name"))
+                throw new Error(
+                    `Each entry in the data must have an name property`
+                );
+            if (!has(entry, "data"))
+                throw new Error(
+                    `Each entry in the data must have an data property`
+                );
+            if (!isPlainObject(entry.data))
+                throw new Error(
+                    `The data property of an entry must be an object`
+                );
+            if (!has(entry.data, "@id"))
+                throw new Error(`The data object must have an @id property`);
+            if (!has(entry.data, "@type"))
+                throw new Error(`The data object must have an @type property`);
+        }
     }
 }
 
--- src/database.spec.js
+++ src/database.spec.js
@@ -9,3 +9,80 @@ test("it should be able to get a handle to an sqlite database", async () => {
     expect(data).toBeDefined();
     await remove(databaseFile);
 });
+
+test.only("verifying input data", () => {
+    const databaseFile = path.join(__dirname, "database.sqlite");
+    const database = new Database({ databaseFile });
+
+    let data = {};
+    expectTestToThrow({ data, message: "Data must be an array" });
+
+    data = ["value"];
+    expectTestToThrow({
+        data,
+        message: "Each entry in the data must be an object",
+    });
+
+    data = [{}];
+    expectTestToThrow({
+        data,
+        message: "Each entry in the data must have an @id property",
+    });
+
+    data = [{ "@id": "1" }];
+    expectTestToThrow({
+        data,
+        message: "Each entry in the data must have an @type property",
+    });
+
+    data = [{ "@id": "1", "@type": "Person" }];
+    expectTestToThrow({
+        data,
+        message: "Each entry in the data must have an name property",
+    });
+
+    data = [{ "@id": "1", "@type": "Person", name: "x" }];
+    expectTestToThrow({
+        data,
+        message: "Each entry in the data must have an data property",
+    });
+
+    data = [{ "@id": "1", "@type": "Person", name: "x", data: "" }];
+    expectTestToThrow({
+        data,
+        message: "The data property of an entry must be an object",
+    });
+
+    data = [{ "@id": "1", "@type": "Person", name: "x", data: {} }];
+    expectTestToThrow({
+        data,
+        message: "The data object must have an @id property",
+    });
+
+    data = [{ "@id": "1", "@type": "Person", name: "x", data: { "@id": "1" } }];
+    expectTestToThrow({
+        data,
+        message: "The data object must have an @type property",
+    });
+
+    data = [
+        {
+            "@id": "1",
+            "@type": "Person",
+            name: "x",
+            data: { "@id": "1", "@type": "Person" },
+        },
+    ];
+    expectTestToThrow({
+        data,
+        message: "The data object must have an @type property",
+    });
+
+    function expectTestToThrow({ data, message }) {
+        try {
+            database.verifyInputData({ data });
+        } catch (error) {
+            expect(error.message).toMatch(message);
+        }
+    }
+});
